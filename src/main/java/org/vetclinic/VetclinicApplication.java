1aac6997676978f148a533ab5b1c042d4388e468
achwi00 <achwierut00@gmail.com>
2024-09-29 15:20:45 +0200

    Initial commit

---
+++ src/main/java/org/vetclinic/VetclinicApplication.java
@@ -0,0 +1,15 @@
+package org.vetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class VetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetclinicApplication.class, args);
+	}
+
+}
