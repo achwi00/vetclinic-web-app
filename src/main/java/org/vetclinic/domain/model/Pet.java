3bd55d30c19ab510c2049e7359f62bc8e102ab03
achwi00 <achwierut00@gmail.com>
2024-12-02 15:46:04 +0100

    [Fix] fixed birt date format in Pet

--- src/main/java/org/vetclinic/domain/model/Pet.java
+++ src/main/java/org/vetclinic/domain/model/Pet.java
@@ -5,18 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
+import java.time.LocalDate;
 
 @Data
 @Entity
 @NoArgsConstructor
 public class Pet extends BasePet{
 
-    public Pet(String name, Type type, String breed, Instant birthDate, User owner, Sex sex){
+    public Pet(String name, Type type, String breed, LocalDate birthDate, User owner, Sex sex){
         super(name, type, breed, owner);
         this.birthDate = birthDate;
         this.sex = sex;
     }
-    private Instant birthDate;
+    private LocalDate birthDate;
 
     public enum Sex{
         MALE("male"), FEMALE("female"), UNKNOWN("unknown");
